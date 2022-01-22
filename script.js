56a994037843d9f475aff4b8b04c0b04bfdcea9a
camilaleite <camilalenas_@hotmail.com>
2022-01-22 20:43:51 -0300

    subindo ex um

---
+++ script.js
@@ -0,0 +1,16 @@
+//EXERCICIOS
+// Crie uma variável que receba um nome;
+let name = "Camila"
+// Crie uma variável que receba um número;
+let number = 13
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let year = 2022
+let yearMinusNumber = year - number
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log (number + yearMinusNumber)
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log ("Estou mexendo no console.log")
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
